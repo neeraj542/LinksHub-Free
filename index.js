1c318b35c37a0eb0b964bc56c1809a2fdc6e84f5
Neeraj Meena IND <114648043+neeraj542@users.noreply.github.com>
2023-08-29 12:21:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+window.addEventListener("DOMContentLoaded", () => {
+  VANTA.BIRDS({
+    el: "#vanta",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.0,
+    minWidth: 200.0,
+    scale: 1.0,
+    scaleMobile: 1.0,
+    backgroundColor: 0xa395e9,
+    color1: 0x0,
+    color2: 0x0,
+    alignment: 77.0,
+    quantity: 2.0,
+  });
+
+  setTimeout(() => {
+    const main = document.querySelector("main");
+    main.style.opacity = 1;
+    main.style.filter = "blur(0px)";
+  }, 100);
+});
